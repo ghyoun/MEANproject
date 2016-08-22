45e3f0d74d750f75dad987d3ef66711e54cdfe7f
ghyoun <ghyoun@uw.edu>
2016-08-22 13:25:02 -0700

    added genomes model

--- server/models/Genome.js
+++ server/models/Genome.js
@@ -1,9 +1,21 @@
 var mongoose = require('mongoose');
 
 var genomeSchema = mongoose.Schema({
-    email: {
+    locus: {
         type: String,
         required: true
+    },
+
+    definition: {
+        type: String,
+        required: true
+    },
+
+    sequence: {
+        type: String,
+        require: true
     }
 
 }, {timestamps: true});
+
+var Genome = mongoose.model('Genome', genomeSchema);
